59793c9e3fb95c82c7e22e9813eea5b63f753048
Saru Manandhar <sarumdr123@gmail.com>
2023-06-21 07:44:30 +0545

    Add axios and fetch in useEffect

--- src/pages/UseEffectExample/index.js
+++ src/pages/UseEffectExample/index.js
@@ -1,14 +1,29 @@
 import { useEffect, useState } from "react"
+import axios from "axios"
 
 const UseEffectExample = () =>{
     const [count,setCount] = useState(0)
     useEffect(()=>{
         console.log('mounting phase')
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>response.json())
+.then((data)=>{
+    console.log(data,'data')
+})
+
+axios.get('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    console.log(response.data)
+})
+
     },[count])
 
     const handleCount = () =>{
         setCount(count+1)
     }
+
+
     return(
         <>
         <p>UseEffectExample</p>
