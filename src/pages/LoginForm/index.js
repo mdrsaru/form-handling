fac1cdb3dba68d84959f4a885bb3383a34cd62d3
Saru Manandhar <sarumdr123@gmail.com>
2023-06-30 07:21:40 +0545

    Add navigate in login page

--- src/pages/LoginForm/index.js
+++ src/pages/LoginForm/index.js
@@ -1,11 +1,12 @@
 import { Form, Button } from "react-bootstrap";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const handleChange = (event) => {
-    console.log( typeof event.target.name)
+    console.log(typeof event.target.name);
     if (event.target.name === "email") {
       setEmail(event.target.value);
     } else {
@@ -15,46 +16,55 @@ const LoginForm = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log("submit");
-    console.log(email,'email');
-    console.log(password,'password');
-
-    
+    console.log(email, "email");
+    console.log(password, "password");
   };
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Enter email"
-          name="email"
-          onChange={handleChange}
-        />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+  const navigate = useNavigate()
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          name="password"
-          onChange={handleChange}
-        />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-      <Button variant="primary" type="reset">
-        Reset
-      </Button>
-    </Form>
+  const handleSignUpClick =() =>{
+    console.log('sign up click')
+    navigate('/register')
+  }
+  return (
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            onChange={handleChange}
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        <Button variant="primary" type="reset">
+          Reset
+        </Button>
+      </Form>
+      <p>
+        Don't Have an account . <span onClick={handleSignUpClick}>Sign up now</span>
+      </p>
+    </>
   );
 };
 
